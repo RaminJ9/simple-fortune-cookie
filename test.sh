0459b9027fea7481119ef6ace23bed5535195fac
Ramin Javed <raminjaved@gmail.com>
2026-08-13 17:15:26 +0200

    ISSUE: #06 - Added the test.sh file

---
+++ test.sh
@@ -0,0 +1,14 @@
+#test file
+
+#Find NodePort automatic
+PORT=$(kubectl get service frontend -o jsonpath='{.spec.ports[0].nodePort}')
+
+curl --fail --silent http://localhost:$PORT > /dev/null
+
+if [ $? -eq 0 ]; then
+  echo "Application is running"
+  exit 0
+else
+  echo "Application is not running"
+  exit 1
+fi
