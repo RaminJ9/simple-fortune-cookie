fa1b55bf99913da3e47967dd1c7e160a9ebac263
Mohammed Hamarash <mohammed_hamarashi@hotmail.com>
2026-08-15 13:25:34 +0200

    ISSUE #04 - Changed deploy file so it uses rollouts

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,9 @@ echo "$KUBECONFIG" | base64 -d > kubeconfig
 
 # Uses the kubeconfig file to deploy the Kubernetes manifests to the cluster.
 kubectl --kubeconfig kubeconfig apply -f kubernetes/
+
+
+kubectl --kubeconfig kubeconfig rollout restart deployment backend
+kubectl --kubeconfig kubeconfig rollout restart deployment frontend
+kubectl --kubeconfig kubeconfig rollout status deployment backend
+kubectl --kubeconfig kubeconfig rollout status deployment frontend
